fee7259df6f168b260160f5f6a398b7875c52103
David Xchel Morales Hurtado <123974833+dxchel@users.noreply.github.com>
2026-08-02 11:17:19 -0600

    Change university tab to college in Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -198,7 +198,7 @@ export const Projects = (): React.ReactElement => {
                                             <Nav.Link eventKey="work">Work</Nav.Link>
                                         </Nav.Item>
                                         <Nav.Item>
-                                            <Nav.Link eventKey="university">University</Nav.Link>
+                                            <Nav.Link eventKey="college">College</Nav.Link>
                                         </Nav.Item>
                                     </Nav>
                                     <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__fadeInLeft" : ""}>
@@ -222,7 +222,7 @@ export const Projects = (): React.ReactElement => {
                                                 }
                                             </Row>
                                         </Tab.Pane>
-                                        <Tab.Pane eventKey="university">
+                                        <Tab.Pane eventKey="college">
                                             <p>In college I made a lot of projects from electrical circuits, microchips, programming, simulation, VR, AR, among others, always delivering top quality and even going the extra mile trying to find better processes or more features for a better product.</p>
                                             <Row>
                                                 {
